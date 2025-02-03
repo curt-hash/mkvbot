e9150f6320d44815c187682a2127ef6fe9c445ea
Curt Hash <curt.hash@gmail.com>
2025-02-03 07:58:11 -0700

    refactor: clean up moviedb pkg

--- pkg/moviedb/moviedb.go
+++ pkg/moviedb/moviedb.go
@@ -1,11 +1,20 @@
 package moviedb
 
-type Metadata struct {
+// MovieMetadata is metadata about a movie.
+//
+// It contains the fields necessary for Plex's file naming scheme:
+// https://support.plex.tv/articles/naming-and-organizing-your-movie-media-files
+type MovieMetadata struct {
 	Name string
 	Year int
-	Tag  string
+
+	// ID is the movie database identifier, e.g., "imdb-tt0118715".
+	ID string
 }
 
-type DB interface {
-	FuzzySearchTitle(q string) ([]*Metadata, error)
+// MovieDB is the interface implemented by movie databases such as IMDb.
+type MovieDB interface {
+	// SearchMovies returns a list of results matching query q (typically the
+	// movie title) ordered by relevance.
+	SearchMovies(q string) ([]*MovieMetadata, error)
 }
